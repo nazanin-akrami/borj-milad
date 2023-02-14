cf2b4e0ca47a825e0064405d24cd71c65050392d
nazanin-akrami <125400330+nazanin-akrami@users.noreply.github.com>
2023-02-14 22:38:23 +0330

    Create milad-tower.cs

---
+++ milad-tower.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("          °");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("          █");
+        Console.WriteLine("          █");
+        Console.WriteLine("          █");
+        Console.WriteLine("          ▀");
+        Console.WriteLine("          ▓");
+        Console.WriteLine("          ▓");
+        Console.WriteLine("          ▓");
+        Console.WriteLine("          ▓");
+        Console.WriteLine("         ▓▓▓");
+        Console.WriteLine("         ▓▓▓");
+        Console.ForegroundColor=ConsoleColor.DarkGreen;
+        Console.WriteLine("         ▓▓▓");
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("        ▓▓▓▓▓");
+        Console.WriteLine("       ▓▓▓▓▓▓▓");
+        Console.WriteLine("      ▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("    ▓▓▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("  ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄");
+        Console.WriteLine("   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("    ▓▓▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("     ▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("      ▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("       ▓▓▓▓▓▓▓");
+        Console.WriteLine("        ▓▓▓▓▓");
+        Console.WriteLine("      ▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("      ▐▐▓▓▓▓▓▐▐");
+        Console.WriteLine("  █████████████████");
+        Console.WriteLine("   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("    ▓▓▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("     ▓▓▓▓▓▓▓▓▓▓▓");
+        Console.WriteLine("      █████████");
+        Console.ReadKey();
+        }
+    }
+}
